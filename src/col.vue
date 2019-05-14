e46f78c6e4798e3559d1a387087466012a4b1262
BarryDong <286084845@qq.com>
2019-05-14 22:16:52 +0800

    row 和 col 的雏形完成

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]">
     <slot></slot>
   </div>
 </template>
@@ -8,6 +8,9 @@
     name: 'col',
     props: {
       span: {
+        type: [Number, String]
+      },
+      offset: {
         type: [Number, String]
       }
     }
@@ -19,11 +22,19 @@
     height: 100px;
     background: gray;
     border: 1px solid red;
+    padding: 0 10px;
 
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
